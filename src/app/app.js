3a7c66c9cd82051a3b1a5ecc8c927162987f6d1c
alexcharlton93 <alex-charlton@live.co.uk>
2020-06-20 19:11:56 +0100

    Working on app.js, removed node_modules from repo, added gitignore

---
+++ src/app/app.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import * as bodyParser from 'body-parser';
+import { databaseService } from '../common/database';
+import { config } from '../common/config';
+import { controllerCatch } from '../common/errors';
+
+async function initApp() {
+    const app = express();
+    
+    // TODO: Should configure routes in a different file
+    var indexRouter = require('./routes/index');
+    var usersRouter = require('./routes/users');
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(cookieParser());
+
+    // remove security risk headers
+	app.disable('x-powered-by');
+	app.disable('etag');
+	app.disable('Server');
+	app.use(
+		helmet({
+			hsts: {
+				maxAge: 31536000,
+				includeSubDomains: true,
+			},
+		}),
+    );
+    
+    app.use(compression());
+
+    // eslint-disable-next-line no-unused-vars
+	app.use((err, request, response, next) => controllerCatch(err, request, response));
+
+	app.listen(config.environment.port, () => {
+		logger.info(`up and running on port: ${config.environment.port}`);
+		logger.info(`enviroment: ${process.env.NODE_ENV}`);
+	});
+
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter);
+
+    await databaseService();
+}
+
+initApp();
