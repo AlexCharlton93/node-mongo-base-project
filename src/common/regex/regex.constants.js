167074c8d0c92667bf5677f8048a965f80f26ffb
alexcharlton93 <alex-charlton@live.co.uk>
2020-06-21 12:50:49 +0100

    Removed unnecessary user properties and user regexes

--- src/common/regex/regex.constants.js
+++ src/common/regex/regex.constants.js
@@ -1,30 +1,9 @@
 export const regex = {
     /* Match 0-9 and A-F only, 8 chars - 4 chars - 4 chars - 4 chars - 12 chars */
     uuidv4: /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
-    /* Match only digits, one or more times, 1 decimal point & negative values */
-    currency: /^-?\d*\.?\d+$/,
-    /* Match a-z any case, period, all numbers, up to 150 characters */
-    imageURL: /^[a-zA-Z0-9.]{0,150}$/,
-    /* Date format in either DD-MM-YYYY or DD/MM/YYYY */
-    date: /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/,
     user: {
-        /* Match a-z in any case, up to 150 characters */
-        firstName: /^[a-zA-Z]{0,150}$/,
-        /* Match a-z in any case, up to 150 characters */
-        surname: /^[a-zA-Z]{0,150}$/,
         // TODO: For now we are keeping this rule pretty open as it's hard to validate an email address
         emailAddress: /\S+@\S+\.\S+/,
-        contactNumber: /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/,
-        /*  Match only a-z characters in any case, 0-9, and limiting the other characters */
-        /* And between 0 and 150 characters in length */
-        addressLine1: /^[a-zA-Z0-9\s,'-.]{0,150}$/,
-        addressLine2: /^[a-zA-Z0-9\s,'-.]{0,150}$/,
-        addressLine3: /^[a-zA-Z0-9\s,'-.]{0,150}$/,
-        country: /^[a-zA-Z0-9\s,'-.]{0,150}$/,
-        /* Match a-z characters in any case, with spaces and numbers allowed */
-        /* Limited to 13 characters */
-        postcode: /^[a-zA-Z0-9'\-\w\s]{0,13}$/,
-        /* Match 1 digit, either 0, 1 or 2 (Customer / Publisher / Admin ) */
         role: /^[0-2]{1}$/,
         /*  Match only a-z characters in any case, 0-9, and limiting the other characters */
         /* And between 0 and 65 characters in length */
--- src/common/services/user/user.model.js
+++ src/common/services/user/user.model.js
@@ -21,71 +21,16 @@ const properties = {
 		default: uuid,
 		validate: regex.uuidv4,
 	},
-	firstName: {
-		type: String,
-		validate: regex.user.firstName,
-	},
-	surname: {
-		type: String,
-		validate: regex.user.surname,
-	},
 	emailAddress: {
 		type: String,
 		required: true,
 		validate: regex.user.emailAddress,
 	},
-	contactNumber: {
-		type: String,
-		validate: regex.user.contactNumber,
-	},
 	password: {
 		type: String,
 		required: true,
-		// TODO: // validate: regex.user.password,
-	},
-	deliveryAddress: {
-		addressLine1: {
-			type: String,
-			validate: regex.user.addressLine1,
-		},
-		addressLine2: {
-			type: String,
-			validate: regex.user.addressLine2,
-		},
-		addressLine3: {
-			type: String,
-			validate: regex.user.addressLine3,
-		},
-		country: {
-			type: String,
-			validate: regex.user.country,
-		},
-		postcode: {
-			type: String,
-			validate: regex.user.postcode,
-		},
-	},
-	billingAddress: {
-		addressLine1: {
-			type: String,
-			validate: regex.user.addressLine1,
-		},
-		addressLine2: {
-			type: String,
-			validate: regex.user.addressLine2,
-		},
-		addressLine3: {
-			type: String,
-			validate: regex.user.addressLine3,
-		},
-		country: {
-			type: String,
-			validate: regex.user.country,
-		},
-		postcode: {
-			type: String,
-			validate: regex.user.postcode,
-		},
+		// TODO: Create password regex
+		//validate: regex.user.password,
 	},
 	role: {
 		type: Number,
