e32eb6c70bd4179abe91e6c9f93926a72eb01330
Alex Charlton <alex-charlton@live.co.uk>
2022-07-16 03:17:20 +0100

    Update forgot password template

--- src/common/email-templates/forgot-password.js
+++ src/common/email-templates/forgot-password.js
@@ -1,6 +1,6 @@
 // TODO: Update to a nicer looking HTML Template
 export const forgotPasswordEmailTemplate = (verificationCode) => {
-	return '<h2 style="font-size: 16px;"> Imposter Game </h2>'
+	return '<h2 style="font-size: 16px;"> Forgot Password </h2>'
 		+ '<p>Click the button below to reset your password</p>'
 		+ `<p>Verification code: ${verificationCode}</p>`
 		+ `<a href="${process.env.APP_RESET_PASSWORD_URL}${verificationCode}">Reset Password</a>`;
